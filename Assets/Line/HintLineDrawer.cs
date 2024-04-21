4e13b3d99c3be2cfa30c8cab5033200961b1cc80
Ilya <ilya1221221@mail.ru>
2024-04-21 16:51:02 +0500

    Line hint

---
+++ Assets/Line/HintLineDrawer.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class HintLineDrawer : MonoBehaviour
+{
+    [SerializeField] private GameObject linePrefab;
+    [SerializeField] private Transform lineParent;
+    [SerializeField] private HintLineSaver saver;
+
+    public void DrawLines()
+    {
+        var lines = saver.GetCurrentLevelHintLines();
+        if (lines != null)
+            foreach (var points in saver.GetCurrentLevelHintLines())
+            {
+                var line = CreateNewLine();
+                foreach (var point in points.List)
+                    line.AddPoint(point);
+            }
+    }
+
+    private Line CreateNewLine()
+    {
+        var currentLine = Instantiate(linePrefab, Vector3.zero, Quaternion.identity).GetComponent<Line>();
+        currentLine.name = "Line";
+        currentLine.transform.SetParent(lineParent);
+        currentLine.SetRigidBodyType(RigidbodyType2D.Kinematic);
+        currentLine.maxPoints = int.MaxValue;
+
+        return currentLine;
+    }
+}
+
+
---
+++ Assets/Line/HintLineSaver.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[CreateAssetMenu(fileName = "HintLineSaver", menuName = "Custom/HintLineSaver", order = 1)]
+public class HintLineSaver : ScriptableObject
+{
+    [SerializeField] private List<LevelHintLine> levelHintLines;
+    [SerializeField] private LastLineSaver saver;
+
+    public void AddLevelHintLines(int levelIndex, List<NestedList<Vector2>> lines)
+    {
+        levelHintLines.RemoveAll(lhl => lhl.LevelInex == levelIndex);
+        levelHintLines.Add(new LevelHintLine(levelIndex, lines));
+    }
+
+    public List<NestedList<Vector2>> GetLevelHintLines(int levelIndex) =>
+        levelHintLines.Find(lhl => lhl.LevelInex == levelIndex).Lines;
+
+    public List<NestedList<Vector2>> GetCurrentLevelHintLines() =>
+        levelHintLines.Find(lhl => lhl.LevelInex == GetCurrentLevelIndex())?.Lines;
+
+    private int GetCurrentLevelIndex() => 
+        int.Parse(SceneManager.GetActiveScene().name.Split(' ')[^1]);
+
+    public void SetSavedLine()
+    {
+        AddLevelHintLines(
+            GetCurrentLevelIndex(), 
+            saver.Lines.Select(line => new NestedList<Vector2> { List = line }).ToList()
+        );
+    }
+}
+
+[System.Serializable]
+class LevelHintLine
+{
+    public int LevelInex => levelIndex;
+    [SerializeField] private int levelIndex;
+
+    public List<NestedList<Vector2>> Lines => lines;
+    [SerializeField] private List<NestedList<Vector2>> lines;
+
+    public LevelHintLine(int levelIndex, List<NestedList<Vector2>> lines)
+    {
+        this.levelIndex = levelIndex;
+        this.lines = lines;
+    }
+}
+
+[System.Serializable]
+public class NestedList<T>
+{
+    public List<T> List;
+}
+
+#if UNITY_EDITOR
+
+[CustomEditor(typeof(HintLineSaver))]
+public class HintLineSaverEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        if (GUILayout.Button("Save current line"))
+        {
+            (target as HintLineSaver).SetSavedLine();
+        }
+    }
+}
+
+#endif
